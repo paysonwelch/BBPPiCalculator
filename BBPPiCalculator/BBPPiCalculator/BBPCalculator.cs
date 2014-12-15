a7c60bdf1fa6733e915b1371806555f4ac171796
paysonwelch <paysonwelch@gmail.com>
2014-12-14 22:58:57 -0500

    Implemented a version of BBP, also spent time understanding what it was calculating, ergo it is calculating a hex stream of characters for Pi instead of the base10 fractional equivalent offset by n, the hex stream has been verified to be correct .

---
+++ BBPPiCalculator/BBPPiCalculator/BBPCalculator.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBPPiCalculator
+{
+    /// <summary>       
+    /// Used to generate the [n]th digit of Pi in base16 (hex). This algorithm creates
+    /// a hex 'slice' that is part of the hexidecimal stream if you were to represent 
+    /// Pi as a hexidecimal number.
+    /// 
+    /// The formulaic code was borrowed from: 
+    /// http://latkin.org/blog/2012/11/03/the-bailey-borwein-plouffe-algorithm-in-c-and-f/
+    /// 
+    /// It conforms to this mathematical formula: 
+    /// http://upload.wikimedia.org/math/d/5/6/d56287522ec15b6d47c9402f24a9ba89.png
+    /// 
+    /// A description of the algorithm can be found in this unrelated vut very 
+    /// informative whitepaper:
+    /// http://crd-legacy.lbl.gov/~dhbailey/dhbpapers/bbp-alg.pdf
+    /// 
+    /// Modifications:
+    ///     - Converted the functional logic to a standardized class structure.
+    ///     - Added more in depth commentary
+    ///     -
+    /// Verified the hexidecimal output by cross referencing this whitepaper:
+    /// http://crd-legacy.lbl.gov/~dhbailey/dhbpapers/bbp-alg.pdf
+    /// 
+    /// Pi in Hexidecimal: 
+    /// http://calccrypto.wikidot.com/math:pi-hex
+    /// 
+    /// Hex positions for debugging
+    /// 0:          243F6A8885A308D313198A2
+    /// 1,000,000:  6C65E52CB459350050E4BB1
+    ///    
+    /// </summary>
+
+    public class BBPCalculator
+    {
+        #region Vars
+        private const int NumHexDigits = 16;                        
+        private const double Epsilon = 1e-17;                        
+        private const int NumTwoPowers = 25;                         
+        private double[] twoPowers = new double[NumTwoPowers];       
+        #endregion
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public BBPCalculator()
+        {
+            this.initializeTwoPowers();
+        }
+
+        /// <summary>
+        /// Calculates the [n]th hexidecimal digit of {i.
+        /// </summary>
+        /// <param name="n">The digit of Pi which you wish to solve for.</param>
+        /// <returns>Returns ten hexidecimal values of Pi from the offset (n).</returns>
+        public string Calc(int n)
+        {
+            double pid, s1, s2, s3, s4;     // summations           
+            string hexDigits;               // the hexidecimal digits
+
+            // calc the summations
+            s1 = Series(1, n);
+            s2 = Series(4, n);
+            s3 = Series(5, n);
+            s4 = Series(6, n);
+            
+            pid = 4d * s1 - 2d * s2 - s3 - s4;          // transform the summations
+            pid = pid - (int)pid + 1d;                  // create the fraction
+            hexDigits = HexString(pid, NumHexDigits);   // convert the fraction to the hex digit slice
+            return hexDigits.Substring(0, 10);          // return 10 hex digits starting at [n] position
+        }
+
+        /// <summary>
+        /// Pre-calculate a power of two table to be used in our calculations.
+        /// </summary>
+        private void initializeTwoPowers()
+        {
+            this.twoPowers[0] = 1d;
+            for (int i = 1; i < NumTwoPowers; i++)
+            {
+                this.twoPowers[i] = 2d * this.twoPowers[i - 1];
+            }
+        }
+
+        /// <summary>
+        /// Converts the fraction to a hexidecimal string.
+        /// </summary>
+        /// <param name="x">The fraction of the summations.</param>
+        /// <param name="numDigits">Number of digits to render to hex.</param>
+        /// <returns>Returns a hex string of length numDigits</returns>
+        private static string HexString(double x, int numDigits)
+        {
+            string hexChars = "0123456789ABCDEF";
+            StringBuilder sb = new StringBuilder(numDigits);
+            double y = Math.Abs(x);
+
+            for (int i = 0; i < numDigits; i++)
+            {
+                y = 16d * (y - Math.Floor(y));
+                sb.Append(hexChars[(int)y]);
+            }
+
+            return sb.ToString();
+        }
+               
+        /// <summary>
+        /// This routine evaluates the series  sum_k 16^(n-k)/(8*k+m)
+        /// using the modular exponentiation technique.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        private double Series(int m, int n)
+        {
+            double denom, pow, sum = 0d, term;
+
+            //  Sum the series up to n.
+            for (int k = 0; k < n; k++)
+            {
+                denom = 8 * k + m;
+                pow = n - k;
+                term = ModPow16(pow, denom);
+                sum = sum + term / denom;
+                sum = sum - (int)sum;
+            }
+
+            //  Compute a few terms where k &gt;= n.
+            for (int k = n; k <= n + 100; k++)
+            {
+                denom = 8 * k + m;
+                term = Math.Pow(16d, (double)(n - k)) / denom;
+                if (term < Epsilon)
+                {
+                    break;
+                }
+                sum = sum + term;
+                sum = sum - (int)sum;
+            }
+
+            return sum;
+        }
+
+        /// <summary>
+        /// Internal helper method.
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="m"></param>
+        /// <returns></returns>
+        private double ModPow16(double p, double m)
+        {
+            int i;
+            double pow1, pow2, result;
+            if (m == 1d)
+            {
+                return 0d;
+            }
+
+            // Find the greatest power of two less than or equal to p.
+            for (i = 0; i < NumTwoPowers; i++)
+            {
+                if (twoPowers[i] > p)
+                {
+                    break;
+                }
+            }
+            pow2 = twoPowers[i - 1];
+            pow1 = p;
+            result = 1d;
+
+            // Perform binary exponentiation algorithm modulo m.
+            for (int j = 1; j <= i; j++)
+            {
+                if (pow1 >= pow2)
+                {
+                    result = 16d * result;
+                    result = result - (int)(result / m) * m;
+                    pow1 = pow1 - pow2;
+                }
+                pow2 = 0.5 * pow2;
+                if (pow2 >= 1d)
+                {
+                    result = result * result;
+                    result = result - (int)(result / m) * m;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- BBPPiCalculator/BBPPiCalculator/Program.cs
+++ BBPPiCalculator/BBPPiCalculator/Program.cs
@@ -7,25 +7,27 @@ using System.Threading.Tasks;
 /******************************************************************************
  * BBP Pi Calculator
  * 2014.12.14
- * Payson Welch; paysonwelch.com, freeideas.org
+ * Payson Welch; paysonwelch.com, freeideas.org, github.com/paysonwelch
+ * paysonwelch@gmail.com
  * 
  * Abstract
  * --------------------------
  * This application is a proof of concept to demonstrate the capability of the 
  * Bailey–Borwein–Plouffe formula which can be used to solve for [n]th binary 
- * digit of pi using base 16 math.
+ * digit in base16 of pi.
  * http://en.wikipedia.org/wiki/Bailey%E2%80%93Borwein%E2%80%93Plouffe_formula
  * 
  * Executive Summary
  * --------------------------------------------------------
  * Originally I was considering using the BBP formula to create a new Proof of
- * Work (POW) system for crypto currencies. With the advent of Proof of Stake 
+ * Work (POW) system for crypto currencies. With the advent of Proof of Stake
  * however and other anonymizing features of currencies the BBP-Pi POW system
  * would not be enough in itself to create a highly successful crypto currency.
+ * 
  * A BBP-Pi POW currency would be more of a novelty of interest to reserachers
  * mostly. There is also PrimeCoin which uses a POW system for hunting chains
  * of prime numbers. So although this is an interesting idea it needs more
- * thought before simply createing a POW based on BBP.
+ * thought before simply creating a POW based on BBP.
  * 
  * This applicaiton has been structured in such a way that it could easily be
  * modified for grid computing. This application currently will not focus on 
@@ -36,28 +38,37 @@ using System.Threading.Tasks;
  *  
  * Bailey–Borwein–Plouffe 
  * --------------------------------------------------------
- * The BBP formula was accidentally discovered in 1995 by researchers. It is a 
- * spigot algorithm which can be used to calculate any digit of Pi.
+ * The BBP formula was accidentally discovered in 1995 but it changed the 
+ * landscape of our understanding of how to calculate certain numbers such
+ * as Pi and e. It uses a series of summations to calculate a hexidecimal 
+ * stream of the floating point portion of Pi.
  * 
  * Compatibility and Crypto Currency Considerations
  * --------------------------------------------------------
- * This proof of concept will not take into account large numbers. As the 
- * dataset grows the max number we wil be able to use in the calculations is
- * 18,446,744,073,709,551,615 which is the value of an unsigned int64. For 
- * crypto currencies it would be possible to use the GNU bignum library.
+ * This applicaiton is not compatible with large numbers, so be cautious about
+ * overflow.
  * 
  * The GMP library has interesting number generators which could also be 
  * introduced for POW systems.
  * https://gmplib.org/pi-with-gmp.html
+ * 
+ * References: 
+ * BBP Algorith for Pi, The (Whitepaper) http://crd-legacy.lbl.gov/~dhbailey/dhbpapers/bbp-alg.pdf
+ * Pi in hexidecimal: http://calccrypto.wikidot.com/math:pi-hex
+ * 
  * ***************************************************************************/
 
 namespace BBPPiCalculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            
-        }
+            BBPCalculator bc = new BBPCalculator();
+            string hd = bc.Calc(0);
+            Console.WriteLine(hd);
+
+            return;
+        }     
     }
 }
