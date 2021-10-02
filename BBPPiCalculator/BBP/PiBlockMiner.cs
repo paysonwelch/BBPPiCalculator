ee2511c8e3271ae130144201cc2ea68547c01713
Jessica Mulein <jessica@mulein.com>
2021-10-02 15:50:05 -0700

    add helper

---
+++ BBPPiCalculator/BBP/PiBlockMiner.cs
@@ -0,0 +1,19 @@
+﻿namespace BBP.FasterKVMiner
+{
+    public static class PiBlockMiner
+    {
+        private static Queue<char> MinedChars;
+        private static PiDigit PiDigit = new PiDigit();
+
+        public async static void MineNext(int count = 10)
+        {
+            var bytesAsync = PiDigit.PiBytesAsync(
+                n: -1,
+                count: count);
+            await foreach (var c in bytesAsync)
+            {
+                MinedChars.Append(c);
+            }
+        }
+    }
+}
